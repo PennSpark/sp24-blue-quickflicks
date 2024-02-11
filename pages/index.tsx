265e0701f0a5bae92e786f98a13fc872a03da9aa
Xue Jiang <haoxuej@seas.upenn.edu>
2024-02-11 16:40:05 -0500

    Connecting front & back end

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+	const handleClick = async () => {
+		try {
+			const response = await fetch("/api/hello");
+			const data = await response.json();
+			console.log(data.message); // Print "hello" to the console
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Hello Next.js!</h1>
+			<button onClick={handleClick}>Click me to say hello!</button>
+		</div>
+	);
+}
