f0dbf6e3f1a48384802efafcdc203043394a0dfb
Xue Jiang <haoxuej@seas.upenn.edu>
2024-04-07 16:29:47 -0400

    Changed Font

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import React from "react";
 import InfoModal from "../modals/InfoModal";
 
 function Header() {
-  return (
-    <header>
-      <h1 className="font-space-mono">Black Connections</h1>
-      <InfoModal />
-    </header>
-  );
+	return (
+		<header>
+			<h1 className="font-rosario">Black Connections</h1>
+			<InfoModal />
+		</header>
+	);
 }
 
 export default Header;
--- src/components/modals/GameLostModal/GameLostModal.js
+++ src/components/modals/GameLostModal/GameLostModal.js
@@ -6,26 +6,25 @@ import CountdownToNextPuzzle from "../../CountdownToNextPuzzle";
 import { PuzzleDataContext } from "../../../providers/PuzzleDataProvider";
 
 function GameLostModal({ open }) {
-  const { gameData } = React.useContext(PuzzleDataContext);
+	const { gameData } = React.useContext(PuzzleDataContext);
 
-  return (
-    <BaseModal
-      title="You lost."
-      initiallyOpen={open}
-      footerElements={<ShareScoreButton />}
-      showActionButton={false}
-    >
-      <div className="grid gap-y-2">
-        <p className="text-lg font-[500] text-center">
-          Better luck next time. The correct answers are below.
-        </p>
-        {gameData.map((obj) => (
-          <SolvedWordRow key={obj.category} {...obj} />
-        ))}
-      </div>
-      <CountdownToNextPuzzle />
-    </BaseModal>
-  );
+	return (
+		<BaseModal
+			title="You lost."
+			initiallyOpen={open}
+			footerElements={<ShareScoreButton />}
+			showActionButton={false}>
+			<div className="grid gap-y-2">
+				<p className="font-rosario text-lg font-[500] text-center">
+					Better luck next time. The correct answers are below.
+				</p>
+				{gameData.map((obj) => (
+					<SolvedWordRow key={obj.category} {...obj} />
+				))}
+			</div>
+			<CountdownToNextPuzzle />
+		</BaseModal>
+	);
 }
 
 export default GameLostModal;
