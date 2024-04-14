df608ebf047dea4edd71604a7aa6b8552045a990
rrenzo <145371176+rrenzo@users.noreply.github.com>
2024-04-14 16:32:33 -0400

    Implemented More Games

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,38 +1,142 @@
-/** @format */
-
 export const CONNECTION_GAMES = [
-	[
-		{
-			category: "Oppenheimer 🍿",
-			words: ["World War II", "Newton", "Cillian Murphy", "2023"],
-			difficulty: 1,
-			imageSrc:
-				"https://movies.universalpictures.com/media/opr-tsr1sheet3-look2-rgb-3-1-1-64545c0d15f1e-1.jpg",
-			link: "https://www.imdb.com/title/tt15398776/?ref_=ext_shr_lnk",
-		},
-		{
-			category: "Coraline 🎬",
-			words: ["Spider", "Button", "Hidden Door", "Henry Selick"],
-			difficulty: 2,
-			imageSrc:
-				"https://image.tmdb.org/t/p/original/nUiumSEJAHtlmtOptoHEbhWJXk9.jpg",
-			link: "https://www.imdb.com/title/tt0327597/?ref_=ext_shr_lnk",
-		},
-		{
-			category: "Eighth Grade 🎥",
-			words: ["Eighth Grade", "Snapchat", "Gucci", "Bo Burnham"],
-			difficulty: 3,
-			imageSrc:
-				"https://www.themoviedb.org/t/p/w1280/lf2IvDRcqKFeAHBLU36ydYg4Sas.jpg",
-			link: "https://www.imdb.com/title/tt7014006/?ref_=ext_shr_lnk",
-		},
-		{
-			category: "Minari 🎞",
-			words: ["Arkansas", "Farm", "Steven Yeun", "Han Yeri"],
-			difficulty: 4,
-			imageSrc:
-				"https://image.tmdb.org/t/p/original/hh8lPultlmaEuoCWB6RsRYIBf04.jpg",
-			link: "https://www.imdb.com/title/tt10633456/?ref_=ext_shr_lnk",
-		},
-	],
+  [
+    {
+      category: "Zone of Interest 🍿",
+      words: ["Auschwitz", "Sound Design", "Sanitation", "Dry Heaving"],
+      difficulty: 1,
+    },
+    {
+      category: "Society of the Snow 🎬",
+      words: ["Rugby", "Flight 571", "Chile", "Andes"],
+      difficulty: 2,
+    },
+    {
+      category: "Moneyball 🎥",
+      words: ["Oakland", "Economics", "Brad Pitt", "Billy Beane"],
+      difficulty: 3,
+    },
+
+    {
+      category: "Megamind 🎞",
+      words: ["Doom Syndicate", "Will Ferell", "Blue", "Metro Man"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "Isle of Dogs 🍿",
+      words: ["Adventure", "Stop Motion", "Wes Anderson", "Japan"],
+      difficulty: 1,
+    },
+    {
+      category: "Cars 🎬",
+      words: ["Ka Chow!", "Pixar", "Piston Cup", "Mater"],
+      difficulty: 2,
+    },
+    {
+      category: "Wonder 🎥",
+      words: ["Auggie", "Fifth Grade", "Cleft Palate", "Julia Roberts"],
+      difficulty: 3,
+    },
+    {
+      category: "Lady Bird 🎞",
+      words: ["Chalamet", "Coming of Age", "Saoirse Ronan", "Like vs. Love"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "Marriage Story 🍿",
+      words: ["Adam Driver", "Theater", "Los Angeles", "Greta Gerwig"],
+      difficulty: 1,
+    },
+    {
+      category: "The Greatest Showman 🎬",
+      words: ["Barnum & Bailey", "Musical", "Zendaya", "This is Me"],
+      difficulty: 2,
+    },
+    {
+      category: "Legally Blonde 🎥",
+      words: ["2001", "Mask of Sanity", "Harvard Law School", "Bend and Snap"],
+      difficulty: 3,
+    },
+    {
+      category: "American Psycho 🎞",
+      words: ["Christian Bale", "Dorsia", "Business Card", "Mask of Sanity"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "Church Leadership Roles",
+      words: ["Deacon", "Elder", "Minister", "Pastor"],
+      difficulty: 1,
+      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/3/Church-Roles.png",
+    },
+    {
+      category: "Synonyms for Lucky",
+      words: ["Blessed", "Fortunate", "Favored", "Providential"],
+      difficulty: 2,
+      imageSrc:
+        "https://d1isxmzxwhhjbk.cloudfront.net/3/Synonyms-for-Lucky.png",
+    },
+    {
+      category: "R&B Icons",
+      words: ["Tyrese", "Usher", "Brandy", "Maxwell"],
+      difficulty: 3,
+      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/3/R-B-Icons.png",
+    },
+
+    {
+      category: "Tupac's Movie Character Names",
+      words: ["Bishop", "Lucky", "Birdie", "Tank"],
+      difficulty: 4,
+      imageSrc: "https://d1isxmzxwhhjbk.cloudfront.net/3/Tupac-Movie-Roles.png",
+    },
+  ],
+  [
+    {
+      category: "Slang for 'A Lot'",
+      words: ["Bare", "Mad", "Hella", "Buku"],
+      difficulty: 1,
+    },
+    {
+      category: "Slang for 'Great'",
+      words: ["Cold", "Raw", "Dope", "Lit"],
+      difficulty: 2,
+    },
+    {
+      category: "Ways to Refer to Money",
+      words: ["Brick", "Band", "Rack", "C-Note"],
+      difficulty: 3,
+    },
+
+    {
+      category: "Things Needed to Make a Pizza",
+      words: ["Cheddar", "Dough", "Sauce", "Heat"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "Oppenheimer 🍿",
+      words: ["World War II", "Newton", "Cillian Murphy", "2023"],
+      difficulty: 1
+    },
+    {
+      category: "Coraline 🎬",
+      words: ["Spider", "Button", "Hidden Door", "Henry Selick"],
+      difficulty: 2
+    },
+    {
+      category: "Eighth Grade 🎥",
+      words: ["Eighth Grade", "Snapchat", "Gucci", "Bo Burnham"],
+      difficulty: 3
+    },
+    {
+      category: "Minari 🎞",
+      words: ["Arkansas", "Farm", "Steven Yeun", "Han Yeri"],
+      difficulty: 4
+    },
+  ],
 ];
